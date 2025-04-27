6bd62bc1a6d8b8776685d9a1d8c98d17714c12be
opi3636 <yoyozohar2010@gmail.com>
2025-04-27 16:25:33 +0300

    remove "-" in arguments

--- DownloadLogs.py
+++ DownloadLogs.py
@@ -3,14 +3,14 @@ import sys
 import os
 
 def PrintHelp():
-    print("Usage: python DownloadLogs.py [OPTIONS...]")
+    print("Usage: python DownloadLogs.py [Options...]")
     print("Options: ")
-    print("-ip      the ip address of the Robot")
-    print("-port    the adb port of the Robot. if not specified will be \"5555\"")
-    print("-local   Local directory to save the logs in")
-    print("-remote  Remote Direcotry with the log files")
-    print("-src     same as \"-remote\"")
-    print("-dst     same as \"-local\"")
+    print("ip      the ip address of the Robot")
+    print("port    the adb port of the Robot. if not specified will be \"5555\"")
+    print("local   Local directory to save the logs in")
+    print("remote  Remote Direcotry with the log files")
+    print("src     same as \"remote\"")
+    print("dst     same as \"local\"")
 
 
 def ParseAurguments():
@@ -25,33 +25,40 @@ def ParseAurguments():
     try:
         for i in range(1, len(sys.argv)):
             match sys.argv[i]:
-                case "-ip":
+                case "ip":
                     DeviceIP = sys.argv[1+i]
-                case "-port":
+                case "port":
                     DevicePort = int(sys.argv[1+i])
-                case "-local":
+                case "local":
                     Local = sys.argv[1+i]
-                case "-remote":
+                case "remote":
                     Remote = sys.argv[1+i]
-                case "-help":
+                case "help":
                     PrintHelp()
                 case "-h":
                     PrintHelp()
-                case "-src":
+                case "src":
                     Remote = sys.argv[1+i]
-                case "-dst":
+                case "dst":
                     Local = sys.argv[1+i]
     except:
         pass
     if len(sys.argv) == 1:
         PrintHelp()
     else:
-        if sys.argv[1] != "-h" or "-help":
+        if sys.argv[1] != "h" or "help":
             if DeviceIP == "" or Local == "" or Remote == "":
                 print("Not all required arguments have been passed!\nExiting..")
                 sys.exit(1)
                 if os.path.exists(Local) and os.path.isdir(Local):
                     print(Local + " Doenst exist or isnt a directory!\nExiting...")
                     sys.exit(2)
+def ConnectAdb():
+    global adb
+    adb = adbutils.AdbClient(DeviceIP, DevicePort)
+    for info in adb.device_list():
+        print(info.serial, info.get_state)
+
 ParseAurguments()
+ConnectAdb()
 print("IP: " + DeviceIP + " Port: " + str(DevicePort) + " Local: " + Local + " Remote: " + Remote)
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Skelloggs
----
+
 Skelloggs is a python script to easily download folders from an adb client. designed specifically for use in FTC.
 ___
 
-### how to install requirements 
+### how to install required packages 
 ```
  pip install -r requirements.txt
 ```
